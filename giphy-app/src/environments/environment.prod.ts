dddbae9af786c80d879bb46bde795afa180d93a5
andyjohn23 <andrewnick906@gmail.com>
2020-09-30 22:26:24 +0300

    updated files

--- giphy-app/src/environments/environment.prod.ts
+++ giphy-app/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  giphyApiKey:'AyndQ13ILJISJ36BgIgzQcgWaIbPktEp',
+  firebaseConfig:{
+    apiKey: "AIzaSyCOs9aWRnyuNzVkhXEHf5u8nszY2N28qYc",
+    authDomain: "giphy-app-2fbb2.firebaseapp.com",
+    databaseURL: "https://giphy-app-2fbb2.firebaseio.com",
+    projectId: "giphy-app-2fbb2",
+    storageBucket: "giphy-app-2fbb2.appspot.com",
+    messagingSenderId: "766684713168",
+    appId: "1:766684713168:web:b6d229f5a400eb35cb8ed5",
+    measurementId: "G-P562RD24JP"
+  }
 };
--- giphy-app/src/environments/environment.ts
+++ giphy-app/src/environments/environment.ts
@@ -1,5 +1,15 @@
 export const environment = {
   production: false,
-  giphyApiKey:'AyndQ13ILJISJ36BgIgzQcgWaIbPktEp'
+  giphyApiKey:'AyndQ13ILJISJ36BgIgzQcgWaIbPktEp',
+  firebaseConfig:{
+    apiKey: "AIzaSyCOs9aWRnyuNzVkhXEHf5u8nszY2N28qYc",
+    authDomain: "giphy-app-2fbb2.firebaseapp.com",
+    databaseURL: "https://giphy-app-2fbb2.firebaseio.com",
+    projectId: "giphy-app-2fbb2",
+    storageBucket: "giphy-app-2fbb2.appspot.com",
+    messagingSenderId: "766684713168",
+    appId: "1:766684713168:web:b6d229f5a400eb35cb8ed5",
+    measurementId: "G-P562RD24JP"
+  }
 };
 
